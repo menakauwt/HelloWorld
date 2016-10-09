70d1d6727df5b1d84d6a3e03b6d83d69985a7c70
menakauwt <mmuppa@yahoo.com>
2016-10-08 22:10:27 -0700

    Update Hello.java again

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Testing pull request");
 		System.out.println("Adding another println");
-
+		System.out.println("Adding another println");
 	}
 
 }
